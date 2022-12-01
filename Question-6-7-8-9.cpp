eafa7757fe45b05a5e814dd7bcd64530c23642f4
wam00732 <wam00732@castleton.edu>
2022-12-01 10:37:46 -0500

    lab 14 not finished

--- Question-6-7-8-9.cpp
+++ Question-6-7-8-9.cpp
@@ -1,39 +1,39 @@
 #include <iostream>
+#include <sstream>
 #include <string>
-#include <sstream> 
-using std::string;
 using std::cout;
 using std::endl;
-
+using std::string;
 
 class Player {
-    private:
-        string name;
-        int hitPoints;
+   private:
+    string name;
+    int hitPoints;
 
-    public:
-        //❓❓ QUESTION 9, add the constructor here:
+   public:
+    //❓❓ QUESTION 9, add the constructor here:
 
-        
+    Player(string n, int h) {
+        name = n;
+        hitPoints = h;
+    }
 
-        string getName(){
-            return name;
-        }
+    string getName() {
+        return name;
+    }
 
-        int getHitPoints(){
-            return hitPoints;
-        }
+    int getHitPoints() {
+        return hitPoints;
+    }
 
-        void damage(int points){
-            if ( points > hitPoints )
-                hitPoints = 0;
-            else
-                hitPoints = hitPoints - points;
-        }
+    void damage(int points) {
+        if (points > hitPoints)
+            hitPoints = 0;
+        else
+            hitPoints = hitPoints - points;
+    }
 };
 
-int main(){
+int main() {
     //❓❓ QUESTION 9, add to the main function here:
-
-
 }
